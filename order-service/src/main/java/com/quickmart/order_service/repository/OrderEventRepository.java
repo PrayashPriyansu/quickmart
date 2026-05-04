09a618b335bb1986392edb188ef4abddb9f53256
Prayash Priyansu <prayash.priyansu.beura.beura@ericsson.com>
2026-05-04 15:19:36 +0530

    feat: add OrderEvent repository

---
+++ order-service/src/main/java/com/quickmart/order_service/repository/OrderEventRepository.java
@@ -0,0 +1,12 @@
+package com.quickmart.order_service.repository;
+
+import com.quickmart.order_service.event.OrderEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderEventRepository extends JpaRepository<OrderEvent, UUID> {
+
+    List<OrderEvent> findByAggregateIdOrderByVersionAsc(UUID aggregateId);
+}
